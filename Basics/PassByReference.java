a1668398cd4b84ba841267706654c000c2e68914
ssm0801 <sudhanshumotewar2001@gmail.com>
2023-09-02 01:07:02 +0530

    learned pass by value and reference

---
+++ Basics/PassByReference.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class PassByReference {
+    static void modifyArray(int[] arr) {
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        // it will print [0, 0, 0]
+        System.out.println(Arrays.toString(arr));
+        modifyArray(arr);
+        // it will print [1, 2, 3]
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ Basics/PassByValue.java
@@ -0,0 +1,11 @@
+public class PassByValue {
+    static void modifyValue(int num) {
+        num = 10;
+    }
+    public static void main(String[] args) {
+        int num = 20;
+        modifyValue(num);
+        // it will print 20
+        System.out.println(num);   
+    }
+}
