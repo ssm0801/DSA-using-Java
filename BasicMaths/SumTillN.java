c69336f2c64f546e156caa48c27d09fb6fcadba9
ssm0801 <sudhanshumotewar2001@gmail.com>
2023-09-06 00:08:16 +0530

    sum till n

---
+++ BasicMaths/SumTillN.java
@@ -0,0 +1,24 @@
+package BasicMaths;
+
+public class SumTillN {
+
+    // TC = O(n)
+    static int sum1(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    // TC = O(1)
+    // using formula n(n+1)/2
+    static int sum2(int n) {
+        return (n * (n + 1)) / 2;
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(sum1(n));
+        System.out.println(sum2(n));
+    }
+}
