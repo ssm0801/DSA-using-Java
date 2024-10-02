247e4e2a2214214c3555a0802d196ba3477925c0
ssm0801 <sudhanshumotewar2001@gmail.com>
2024-10-02 16:27:40 +0530

    arrays modifed

--- Basics/Array.java
+++ Basics/Array.java
@@ -1,5 +1,3 @@
-package Basics;
-
 import java.util.Arrays;
 
 public class Array {
@@ -12,16 +10,16 @@ public class Array {
         // default values are 0
         int[] arr3 = new int[5];
 
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+        System.out.println(Arrays.toString(arr3));
+
         int[][] arr4 = { {1, 2}, {3, 4, 5} };
         int[][] arr5= new int[][] { {1, 2, 3}, {4}, {5} };
         // default values are 0
         int[][] arr6 = new int[5][4];
         // wont have default values, each row will be null
         int[][] arr7 = new int[5][];
-
-        System.out.println(Arrays.toString(arr1));
-        System.out.println(Arrays.toString(arr2));
-        System.out.println(Arrays.toString(arr3));
         
         for (int[] row : arr4) {
             System.out.println(Arrays.toString(row));
