611c16679a63f97ef06acd9a9d0a344c620b975f
ssm0801 <sudhanshumotewar2001@gmail.com>
2024-10-10 02:50:56 +0530

    selection sort completed

---
+++ Sorting/Selection_Sort/README.md
@@ -0,0 +1,13 @@
+# Selection Sort
+
+- Select minimum and swap
+- Time complexity is `O(n^2)`
+
+```java
+int[] arr = {13, 46, 24, 52, 20, 9};
+```
+
+![Image]()
+
+- From above image its clear that outer loop `i` will run from 0 to n-2 i.e `[0,n-2]`
+- For inner loop we will start from `i` till `n-1` every time to find minimum i.e `[i,n-1]`
---
+++ Sorting/Selection_Sort/SelectionSort.java
@@ -0,0 +1,34 @@
+package Sorting.Selection_Sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+  // select minimum and swap
+  // O(n^2)
+  static int[] selectionSort(int[] arr) {
+    int n = arr.length;
+    for (int i = 0; i <= n - 2; i++) {
+      int min = i;
+      for (int j = i; j <= n - 1; j++) {
+        if (arr[j] < arr[min]) {
+          min = j;
+        }
+      }
+      swap(arr, i, min);
+    }
+    return arr;
+  }
+
+  static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = { 13, 46, 24, 52, 20, 9 };
+    int[] arrSorted = selectionSort(arr);
+    System.out.println(Arrays.toString(arrSorted));
+  }
+}
