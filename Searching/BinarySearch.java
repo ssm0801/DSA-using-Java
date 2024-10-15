eeb236be797ec7a972635d378dce7d0b3b1c1db2
Sudhanshu Motewar <46257802+ssm0801@users.noreply.github.com>
2024-10-16 00:38:01 +0530

    added recursive binary search

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -1,4 +1,6 @@
 public class BinarySearch {
+    
+    // O(log n)
     static int binarySearch(int[] arr, int target) {
         int low = 0;
         int high = arr.length - 1;
@@ -16,10 +18,27 @@ public class BinarySearch {
         }
         return -1;
     }
+    
+    // recursive
+    // O(log n)
+    static int binarySearch1(int[] arr, int target, int low, int high) {
+        if (low > high) {
+            return -1;
+        }
+        int mid = (low + high) / 2;
+        if (target > arr[mid]) {
+            return binarySearch1(arr, target, mid + 1, high);
+        }
+        else if (target < arr[mid]) {
+            return binarySearch1(arr, target, low, mid - 1);
+        }
+        return mid;
+    }
+    
     public static void main(String[] args) {
         int[] arr = {7, 8, 9, 13, 20, 24, 26, 32, 46, 52, 75};
         int target = 46;
-        int result = binarySearch(arr, target);
+        int result = binarySearch1(arr, target, 0, arr.length - 1);
         if (result == -1) {
             System.out.println("Element not found");
         } else {
