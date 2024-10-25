934c8d68e993a833352f55b3e4a75e9f977fcdcb
ssm0801 <sudhanshumotewar2001@gmail.com>
2024-10-26 00:09:24 +0530

    revised sorting and searching

--- Searching/LinearSearch.java
+++ Searching/LinearSearch.java
@@ -1,23 +1,23 @@
-public class LinearSearch
-{
-    // O(n)
-    static int linearSearch(int[] arr, int n) {
-        for (int i=0; i<arr.length; i++) {
-	        if (arr[i] == n) {
-	            return i;
-	        }
-	    }
-	    return -1;
-    }
+public class LinearSearch {
+
+	// O(n)
+	static int linearSearch(int[] arr, int n) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == n) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
 	public static void main(String[] args) {
-	    int[] arr = {13, 46, 24, 52, 20, 9, 75, 26, 7, 8, 32};
-	    int n = 78;
-	    int index = linearSearch(arr, n);
-	    if (index != -1) {
-	        System.out.println(n + " is present at " + index);
-	    }
-	    else {
-	        System.out.println(n + " is not present");
-	    }
+		int[] arr = { 13, 46, 24, 52, 20, 9, 75, 26, 7, 8, 32 };
+		int n = 78;
+		int index = linearSearch(arr, n);
+		if (index != -1) {
+			System.out.println(n + " is present at " + index);
+		} else {
+			System.out.println(n + " is not present");
+		}
 	}
 }
