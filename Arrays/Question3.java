6170ee1b454f9a1d6b8dba547fd5701a91262edf
ssm0801 <sudhanshumotewar2001@gmail.com>
2024-10-28 00:41:28 +0530

    naming convection

--- Arrays/Question3.java
+++ Arrays/Question3.java
@@ -7,11 +7,28 @@ import java.util.Set;
 
 public class Question3 {
 
+  // bruteforce
+  // can have new array with unique elements
+  // SC : O(k) ; k unique elements
+  // TC : O(n)
+  public static int[] removeDuplicates1(int[] arr) {
+    Set<Integer> set = new HashSet<>();
+    for (Integer value : arr) {
+      set.add(value);
+    }
+    Iterator<Integer> itr = set.iterator();
+    while (itr.hasNext()) {
+      set.add(itr.next());
+    }
+    return set.stream().mapToInt(Integer::intValue).toArray();
+  }
+
+  // optimised
   // remove duplicates in same array and take remaining numbers as -1
   // two pointer
   // SC : O(1)
   // TC : O(n)
-  public static void removeDuplicates1(int[] arr) {
+  public static void removeDuplicates2(int[] arr) {
     int i = 0;
     int j = 0;
     while (j < arr.length) {
@@ -26,25 +43,10 @@ public class Question3 {
     }
   }
 
-  // can have new array with unique elements
-  // SC : O(k) ; k unique elements
-  // TC : O(n)
-  public static int[] removeDuplicates2(int[] arr) {
-    Set<Integer> set = new HashSet<>();
-    for (Integer value : arr) {
-      set.add(value);
-    }
-    Iterator<Integer> itr = set.iterator();
-    while (itr.hasNext()) {
-      set.add(itr.next());
-    }
-    return set.stream().mapToInt(Integer::intValue).toArray();
-  }
-
   public static void main(String[] args) {
     int[] arr = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
-    // int[] removedDuplicates = removeDuplicates2(arr);
-    removeDuplicates1(arr);
+    // int[] removedDuplicates = removeDuplicates1(arr);
+    removeDuplicates2(arr);
     System.out.println("Removed duplicates = " + Arrays.toString(arr));
   }
 }
